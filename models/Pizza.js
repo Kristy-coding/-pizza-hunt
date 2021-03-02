f9233b69aa47a170174ce30723f9ae9989fb4527
Kristy Zurmuhlen <kristyzurmuhlen@gmail.com>
2021-03-02 16:05:03 -0500

    added comment model

--- models/Pizza.js
+++ models/Pizza.js
@@ -24,8 +24,41 @@ const PizzaSchema = new Schema({
       type: String,
       default: 'Large'
     },
-    toppings: []
-  });
+    toppings: [],
+    
+    // CREATE ASSOCIATION, associate Pizza and Commment Models 
+    //we need to tell Mongoose to expect an ObjectId and to tell it that its data comes from the Comment model.
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        //The ref property is especially important because it tells the Pizza model which documents to search to find the right comments
+        ref: 'Comment'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    //We set id to false because this is a virtual that Mongoose returns, and we don’t need it.
+    id:false
+}
+  
+);
+
+//--------- ADD VIRTUAL (aka virtual properties) to the PizzaSchema-----------//
+
+//Virtuals allow you to add virtual properties to a document that aren't stored in the database. They're normally computed values that get evaluated when you try to access their properties
+// get total count of comments and replies on retrieval
+//Virtuals allow us to add more information to a database response so that we don't have to add in the information manually with a helper before responding to the API request
+
+//need to do one more thing—we need to tell the schema that it can use virtuals.
+//To do so, you'll need to add the toJSON property to the schema options
+
+// this virtual gets the length of the comment array 
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+});
 
 //Now we need to actually create the model to get the prebuilt methods that Mongoose provides. Let's add the following code to create the model and export it at the bottom of Pizza.js:
 
